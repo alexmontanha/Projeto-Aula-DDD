0d462f86f12c979b22d1413601797719c06bfaae
Alexandre de Oliveira <alexmontanha@hotmail.com>
2023-05-23 09:10:52 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto Aula DDD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Aula_DDD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
